9548847a79e3a93a08490eb4543c3cde96f6c63d
Mashruf Ahmed <mashrufahmed03@gmail.com>
2025-03-01 13:54:18 +0600

    date and page section update

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Theme section
 let body = document.querySelector('body');
 const themeColors = [
   'aliceblue',
@@ -16,3 +17,31 @@ function themeButton() {
   let chosenColor = themeColors[Math.floor(Math.random() * themeColors.length)];
   body.style.backgroundColor = chosenColor;
 }
+
+function chengePage() {
+  window.location = './pages/questionPage.html';
+}
+
+// Data Section
+let day = document.querySelector('#day');
+let fullDate = document.querySelector('#fullDate');
+const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const months = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+const date = new Date();
+day.innerHTML = `${days[date.getDay()]},`;
+fullDate.innerHTML = `${months[date.getMonth()]} ${
+  date.getDate() < 10 ? '0' + date.getDate() : date.getDate()
+} ${date.getFullYear()}`;
